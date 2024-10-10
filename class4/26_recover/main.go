1f33d5dc2246386267adb8de7d639fc500bdf99b
prakashkaruppusamy <prakashbtech87@gmail.com>
2024-10-11 01:50:42 +0530

    GoLang 3

--- class4/26_recover/main.go
+++ class4/26_recover/main.go
@@ -2,20 +2,27 @@ package main
 
 import "fmt"
 
+// Function that intentionally triggers a panic
 func mayPanic() {
-	panic("a problem occured")
+	panic("a problem occurred") // Trigger a panic with a custom error message
 }
 
 func main() {
+	// Print a message before calling mayPanic
 	fmt.Println("before mayPanic...")
 
+	// Defer a function to recover from a panic
 	defer func() {
+		// Attempt to recover from the panic
 		if r := recover(); r != nil {
-			fmt.Println("recovered from error", r)
+			// If recovery is successful, print the recovered message
+			fmt.Println("recovered from error:", r)
 		}
 	}()
 
+	// Call the mayPanic function, which will cause a panic
 	mayPanic()
 
+	// This line will not execute because mayPanic causes a panic
 	fmt.Println("after mayPanic...")
 }
